e896bfcd5a77e0830f039e4ee933c720336d4dcf
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-11-08 20:29:08 +0100

    .

---
+++ public/api01.php
@@ -0,0 +1,4 @@
+<?php
+include 'keys.php';
+echo 1+$x;
+?>
---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  var x
+  import { onMount } from 'svelte';
+  onMount(async () => {
+    let data = await fetch('/api01.php').then(res => res.text());
+    x = data;
+  })
+</script>
+
+<main>
+  {x}
+</main>
+
+<style>
+ 
+</style>
